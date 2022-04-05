e8cba136787ba7fca0a8e5ed349f7117ecdc4dd0
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-04-05 22:12:26 +0700

    Update printAllCourses method

--- src/printFunction.java
+++ src/printFunction.java
@@ -1,15 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class printFunction{
     StudentEnrollmentList enrollmentList = new StudentEnrollmentList();
     int count = 0;
-    void printAllCourses(String semester){ // printt all course for 1 semester
-        for(StudentEnrollment courses : enrollmentList.getAll()) {
-            if (courses.getSemester().equals(semester)) {
-                System.out.println(courses);
-                count = count + 1;
+
+    void printAllCourses(String semester,String FilePath){ // printt all course for 1 semester. get from default.csv
+        String line ="";
+        int count = 0;
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(FilePath));
+            while((line = br.readLine()) != null){
+                String[] values = line.split(",");
+                if(values[6].equals(semester)){
+                    System.out.println("Course id: "+values[3]+" | Course name: "+values[4]+" | Number of Credits: "+values[5]);
+                    count = count + 1;
+                }
             }
-        }
-        if(count == 0){
-            System.out.println("Found 0 course in this semester");
+            System.out.println("Found "+count+" course/courses in semester "+semester);
+        } catch (IOException e){
+            e.printStackTrace();
         }
     }
     void printCoursesForStudent(String sID,String semester){ //print all course for 1 student in 1 semester
