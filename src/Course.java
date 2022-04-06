3096fea445828ddffdc445a313a471629caafde3
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-04-06 13:48:37 +0700

    Final Update of Course

--- src/Course.java
+++ src/Course.java
@@ -1,39 +1,30 @@
 public class Course {
     private String c_id;
     private String c_name;
-    private int noOfCredits;
-
+    private String noOfCredits;
 
     public String getC_id() {
         return c_id;
     }
-
     public void setC_id(String c_id) {
         this.c_id = c_id;
     }
-
     public String getC_name() {
         return c_name;
     }
-
     public void setC_name(String c_name) {
         this.c_name = c_name;
     }
-
-    public int getNoOfCredits() {
+    public String getNoOfCredits() {
         return noOfCredits;
     }
-
-    public void setNoOfCredits(int noOfCredits) {
+    public void setNoOfCredits(String noOfCredits) {
         this.noOfCredits = noOfCredits;
     }
-
     @Override
     public String toString() {
-        return "Course{" +
-                "Course ID='" + c_id + '\'' +
-                ", Course Name='" + c_name + '\'' +
-                ", noOfCredits=" + noOfCredits +
-                '}';
+        return "Course ID = " + c_id + " | " +
+                " Course Name = " + c_name + " | " +
+                " noOfCredits = " + noOfCredits;
     }
 }
