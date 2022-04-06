d89542569b852498926aeedeb73cc4dc355d4f6a
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-04-06 13:51:24 +0700

    Final update of printFunction

--- src/printFunction.java
+++ src/printFunction.java
@@ -1,21 +1,26 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class printFunction{
     StudentEnrollmentList enrollmentList = new StudentEnrollmentList();
     int count = 0;
-
-    void printAllCourses(String semester,String FilePath){ // printt all course for 1 semester. get from default.csv
-        String line ="";
+    void printAllCourses(String semester,String FilePath){ // print all course for 1 semester. get from default.csv
+        String line;
+        List<String> courseInSemester = new ArrayList<>();
         int count = 0;
         try{
             BufferedReader br = new BufferedReader(new FileReader(FilePath));
             while((line = br.readLine()) != null){
                 String[] values = line.split(",");
                 if(values[6].equals(semester)){
-                    System.out.println("Course id: "+values[3]+" | Course name: "+values[4]+" | Number of Credits: "+values[5]);
-                    count = count + 1;
+                    if(!courseInSemester.contains(values[3])){
+                        System.out.println("Course id: "+values[3]+" | Course name: "+values[4]+" | Number of Credits: "+values[5]);
+                        count = count + 1;
+                        courseInSemester.add(values[3]);
+                    }
                 }
             }
             System.out.println("Found "+count+" course/courses in semester "+semester);
@@ -24,10 +29,10 @@ public class printFunction{
         }
     }
     void printCoursesForStudent(String sID,String semester){ //print all course for 1 student in 1 semester
-        for(StudentEnrollment Scourses : enrollmentList.getAll()) {
-            if (Scourses.getSemester().equals(semester)) { //if semester equal
-                if(Scourses.getStudent().getS_id().equals(sID)){
-                    System.out.println(Scourses);
+        for(StudentEnrollment enrollment : enrollmentList.getAll()) {
+            if(enrollment.getStudent().getS_id().equals(sID)){
+                if (enrollment.getSemester().equals(semester)) { //if semester equal
+                    System.out.println(enrollment.getCourse().toString());
                     count = count + 1;
                 }
             }
@@ -37,10 +42,10 @@ public class printFunction{
         }
     }
     void printStudentsInCourse(String course,String semester){ //print all student in 1 course
-        for(StudentEnrollment student : enrollmentList.getAll()) {
-            if (student.getSemester().equals(semester)) { //if semester equal
-                if(student.getCourse().getC_id().equals(course)){
-                    System.out.println(student);
+        for(StudentEnrollment enrollment : enrollmentList.getAll()) {
+            if (enrollment.getSemester().equals(semester)) { //if semester equal
+                if(enrollment.getCourse().getC_id().equals(course)){
+                    System.out.println(enrollment.getStudent().toString());
                     count = count + 1;
                 }
             }
