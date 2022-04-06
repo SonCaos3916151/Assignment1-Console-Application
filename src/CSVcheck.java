7b580192ada7831537c32a8cd603058b4adfcceb
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-04-06 13:48:07 +0700

    Final Update of CSVcheck

--- src/CSVcheck.java
+++ src/CSVcheck.java
@@ -1,29 +1,9 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CSVcheck { //input all needed elements from csv file into arraylist to check and get.
-    public List<String> AllsID = new ArrayList<>();
-    public List<String>AllsCourse = new ArrayList<>();
-    public List<String>AllsSemester = new ArrayList<>();
-
     String line = "";
-    void convertCSVtoList(String FilePath){
-        try{
-            BufferedReader br = new BufferedReader(new FileReader(FilePath));
-            while((line = br.readLine()) != null){
-                String[] values = line.split(",");
-                AllsID.add(values[0]);
-                AllsCourse.add(values[3]);
-                AllsSemester.add(values[6]);
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
     boolean checkWithCSV(String tempStudentID, String tempSemester, String tempCourseID,String FilePath){
         try{
             BufferedReader br = new BufferedReader(new FileReader(FilePath));
