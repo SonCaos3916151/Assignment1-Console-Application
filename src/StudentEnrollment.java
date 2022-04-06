e6bef473d7e3d5ccb215625202762c57f6f8f911
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-04-06 22:44:58 +0700

    Update StudentEnrollment.java

--- src/StudentEnrollment.java
+++ src/StudentEnrollment.java
@@ -1,4 +1,3 @@
-
 public class StudentEnrollment {
     private Student student; //from Student Class
     private Course course; //from Course Class
@@ -9,29 +8,16 @@ public class StudentEnrollment {
         this.course = course;
         this.semester = semester;
     }
-
     public Student getStudent() {
         return student;
-    }
-
-    public void setStudent(Student student) {
-        this.student = student;
     }
 
     public Course getCourse() {
         return course;
     }
 
-    public void setCourse(Course course) {
-        this.course = course;
-    }
-
     public String getSemester() {
         return semester;
-    }
-
-    public void setSemester(String semester) {
-        this.semester = semester;
     }
 
     @Override
